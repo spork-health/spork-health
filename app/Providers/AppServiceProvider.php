86b386c6b525d5c313c63c809c7e4abbbd9f1e16
Eugene Triguba <eugenetriguba@gmail.com>
2020-11-01 00:10:45 -0500

    Remove force HTTPS scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (config('app.env') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        //
     }
 
     /**
