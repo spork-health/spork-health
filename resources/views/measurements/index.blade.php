083edec6c7cfa70c96523bfe383cf20cf54c27ae
Eugene Triguba <eugenetriguba@gmail.com>
2020-11-01 01:12:36 -0600

    Add empty measurement image & tabulate measurement data

--- resources/views/measurements/index.blade.php
+++ resources/views/measurements/index.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between align-middle">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                <a href="{{ route('measurements.index') }}">
+        <div class="flex justify-between">
+            <a href="{{ route('measurements.index') }}" class="flex items-center">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                     {{ __('Measurements for ' . Auth::user()->currentTeam->name) }}
-                </a>
-            </h2>
+                </h2>
+            </a>
 
             {{--
             You don't normally want an a tag around a button because
@@ -14,9 +14,7 @@
             --}}
             @if (Auth::user()->hasTeamPermission(Auth::user()->currentTeam, 'create'))
                 <a href="/measurements/create">
-                    <x-jet-button>
-                        {{ __('Log Measurement') }}
-                    </x-jet-button>
+                    <x-jet-button class="font-extrabold text-xl">+</x-jet-button>
                 </a>
             @endif
         </div>
@@ -24,15 +22,48 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
-                @foreach ($measurements as $m)
-                    <p>{{ $m->measurementType->name . ' - ' . $m->value . ' ' . $m->measurementType->unit->name }}</p>
-                    <p>Logged: {{ date('M jS, Y', strtotime($m->log_date)) }}</p>
-                    @if (!$loop->last)
-                        <br />
-                    @endif
-                @endforeach
-            </div>
+            @if (count($measurements) === 0)
+                <div class="text-center">
+                    <img src="{{ asset('images/empty-page.svg') }}" class="w-96 mx-auto" />
+                    <p class="p-4 antialiased text-gray-800 text-xl font-semibold">
+                        Add a new measurement to get started.
+                    </p>
+                    <x-jet-button>Log Measurement</x-jet-button>
+                </div>
+            @else
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
+
+                    <header class="grid grid-cols-3 bg-gray-100 rounded-t-lg border-b-2 border-gray-300">
+                        <div class="col-span-1 h-12 uppercase tracking-wide text-gray-700 font-bold
+                                text-sm antialiased pt-3 ml-6">
+                            Measurement
+                        </div>
+                        <div class="col-span-1 h-12 uppercase tracking-wide text-gray-700 font-bold
+                                text-sm antialiased pt-3">
+                            Value
+                        </div>
+                        <div class="col-span-1 h-12 uppercase tracking-wide text-gray-700 font-bold
+                                text-sm antialiased pt-3">
+                            Log Date
+                        </div>
+                    </header>
+
+
+                    @foreach ($measurements as $m)
+                        <div class="grid grid-cols-3 py-6 border-b-2 hover:bg-gray-100">
+                            <div class="col-span-1 h-12 text-gray-700 antialiased pt-3 ml-6">
+                                {{ $m->measurementType->name }}
+                            </div>
+                            <div class="col-span-1 h-12 text-gray-700 antialiased pt-3">
+                                {{ $m->value . ' ' . $m->measurementType->unit->name }}
+                            </div>
+                            <div class="col-span-1 h-12 text-gray-700 antialiased pt-3">
+                                {{ date('M jS, Y', strtotime($m->log_date)) }}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
