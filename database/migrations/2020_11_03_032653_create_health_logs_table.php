9f697c78f26c85fdb63ce56537a022b6fab6261c
Eugene Triguba <eugenetriguba@gmail.com>
2020-11-02 21:33:23 -0600

    Add health logs db table

---
+++ database/migrations/2020_11_03_032653_create_health_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('health_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id');
+            $table->dateTimeTz('log_date');
+            $table->text('text');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
